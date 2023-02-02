a5bbd823baf969c983976fae5b5f3fa315547761
aldojreateguip <aldo.reategui.0298@gmail.com>
2023-02-01 21:23:34 -0500

    add crud

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -13,6 +13,7 @@
     <title>@yield('title')</title>
 
     <!-- Scripts -->
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
