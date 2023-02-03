1f96edf6a8982a1eec9757992d197d54f32f087a
aldojreateguip <aldo.reategui.0298@gmail.com>
2023-02-02 22:13:57 -0500

    adding asset's config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,30 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import path from 'path'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
+import { config } from "process";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/forms.js'
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/forms.js",
             ],
-            refresh: true,
+            refresh: [{
+                paths: ["public/assets/**"], 
+                config: { delay: 300 } 
+            }],
         }),
     ],
-    resolve:{
-        alias:{
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-            '~bootstrap-icons': path.resolve(__dirname, 'node_modules/bootstrap-icons'),
-            '$': 'jQuery', 
-        }
+    resolve: {
+        alias: {
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+            "~bootstrap-icons": path.resolve(
+                __dirname,
+                "node_modules/bootstrap-icons"
+            ),
+            $: "jQuery",
+        },
     },
 });
