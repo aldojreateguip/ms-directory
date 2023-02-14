3642f1d30bf7be6e4f93c929c61d3788393764e3
aldojreateguip <aldo.reategui.0298@gmail.com>
2023-02-14 14:05:37 -0500

    fix css

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,13 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--Bootsrap 4 CDN-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <!--Fontawesome CDN-->
-    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"> -->
-    <!-- <link rel="stylesheet" href="https://kit.fontawesome.com/21366aa9f0.css" crossorigin="anonymous"> -->
     <title>@yield('title')</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css', 'resources/css/login_styles.css', 'resources/css/header_styles.css','resources/css/sidebar_styles.css'])
+    @vite(['resources/js/app.js', 'resources/css/app.css', 'resources/css/login_styles.css', 'resources/css/header_styles.css'])
 
 </head>
 
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,67 +5,109 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  @vite(['resources/js/app.js'])
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
   <title>Document</title>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/css/adminlte.min.css"></script> -->
+
 </head>
 
-<body>
-  @vite(['resources/js/app.js','resources/css/style.css'])
-  <aside class="sidebar">
-    <div class="toggle">
-      <a href="#" class="burger js-menu-toggle" data-toggle="collapse" data-target="#main-navbar">
-        <span></span>
+<body class="sidebar-mini" style="height: auto;">
+
+  <div class="wrapper">
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <a href="/" class="brand-link">
+        <span class="brand-text font-weight-light">Admin</span>
       </a>
-    </div>
-    <div class="side-inner">
-
-      <div class="profile">
-        <img src="" alt="Image" class="img-fluid">
-        <h3 class="name">Craig David</h3>
-        <span class="country">Web Designer</span>
-      </div>
-
-
-      <div class="nav-menu">
-        <ul>
-          <li class="accordion">
-            <a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne" class="collapsible">
-              <span class="icon-home mr-3"></span>Feed
-            </a>
-            <div id="collapseOne" class="collapse" aria-labelledby="headingOne">
-              <div>
-                <ul>
-                  <li><a href="#">News</a></li>
-                  <li><a href="#">Sport</a></li>
-                  <li><a href="#">Health</a></li>
-                </ul>
+      <div class="sidebar">
+        <div class="user-panel mt-3 pb-3 d-flex">
+          <div class="info">
+            <a href="#" class="d-block">User</a>
+          </div>
+        </div>
+        <div class="form-inline">
+          <div class="input-group" data-widget="sidebar-search">
+            <input type="search" class="form-control form-control-sidebar" placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+              <button class="btn btn-sidebar">
+                <i class="bi bi-search"></i>
+              </button>
+            </div>
+            <div class="sidebar-search-results">
+              <div class="list-group">
+                <a href="#" class="list-group-item">
+                  <div class="search-title">
+                    <strong class="text-light"></strong>
+                    N
+                    <strong class="text-light"></strong>
+                    o<strong class="text-light"></strong>
+                    <strong class="text-light"></strong>
+                    e<strong class="text-light"></strong>
+                    l<strong class="text-light"></strong>
+                    e<strong class="text-light"></strong>
+                    m<strong class="text-light"></strong>
+                    e<strong class="text-light"></strong>
+                    n<strong class="text-light"></strong>
+                    t<strong class="text-light"></strong>
+                    <strong class="text-light"></strong>
+                    f<strong class="text-light"></strong>
+                    o<strong class="text-light"></strong>
+                    u<strong class="text-light"></strong>
+                    n<strong class="text-light"></strong>
+                    d<strong class="text-light"></strong>
+                    !<strong class="text-light"></strong>
+                  </div>
+                </a>
               </div>
             </div>
-          </li>
-          <li class="accordion">
-            <a href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" class="collapsible">
-              <span class="icon-search2 mr-3"></span>Explore
-            </a>
-
-            <div id="collapseTwo" class="collapse" aria-labelledby="headingOne">
-              <div>
-                <ul>
-                  <li><a href="#">Interior</a></li>
-                  <li><a href="#">Food</a></li>
-                  <li><a href="#">Travel</a></li>
-                </ul>
-              </div>
-            </div>
-
-          </li>
-          <li><a href="#"><span class="icon-notifications mr-3"></span>Notifications</a></li>
-          <li><a href="#"><span class="icon-location-arrow mr-3"></span>Direct</a></li>
-          <li><a href="#"><span class="icon-pie-chart mr-3"></span>Stats</a></li>
-          <li><a href="#"><span class="icon-sign-out mr-3"></span>Sign out</a></li>
-        </ul>
+          </div>
+        </div>
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item menu-is-opening menu-open">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-edit"></i>
+                <p>
+                  Forms
+                  <i class="fas fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview" style="display: block;">
+                <li class="nav-item">
+                  <a href="pages/forms/general.html" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>General Elements</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Advanced Elements</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Editors</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Validation</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </div>
+    </aside>
 
-  </aside>
+  </div>
 </body>
 
 </html>
