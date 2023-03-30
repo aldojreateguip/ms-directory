904f60086d238339e248bb8596f262aeefe03724
aldojreateguip <aldo.reategui.0298@gmail.com>
2023-03-30 12:12:16 -0500

    clear controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,13 +8,5 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    public function __construct()
-    {
-        $user_auth = Auth::user();
-        if (!$user_auth) {
-            return redirect()->intended('/');
-        }else{
-            return view('user_view.main');
-        }
-    }
+    
 }
