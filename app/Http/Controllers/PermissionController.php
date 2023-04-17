bc352f048fd9bb89cf78f7f889144384a1bbbb86
aldojreateguip <aldo.reategui.0298@gmail.com>
2023-04-17 11:33:00 -0500

    config show permission list

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -17,13 +18,33 @@ class PermissionController extends Controller
             return view('user_view.main');
         }
     }
-    public function add_permission(){
-        
-    }
-    public function update_permission(){
+    public function show()
+    {
+        $role_description = request('role_description');
 
-    }
-    public function asign_permission(){
+        $role = DB::table('role')
+            ->where('role_description', $role_description)
+            ->first();
 
+        if (empty($role)) {
+            $permissions = Permission::all();
+        } else {
+            $role_id = $role->role_id;
+            $permissions = DB::table('permission as p')
+                ->select('p.*')
+                ->whereNotIn('p.permission_id', function ($query) use ($role_id) {
+                    $query->select('permission_id')
+                        ->from('role_permission')
+                        ->where('role_id', '=', $role_id);
+                })
+                ->get();
+        }
+        return view('layout.show_permission_list', compact('permissions'));
+    }
+    public function update_permission()
+    {
+    }
+    public function asign_permission()
+    {
     }
 }
