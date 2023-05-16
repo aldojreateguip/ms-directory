a7e5a960a5d3f292d1868d5c03ef220635e5d4ce
aldojreateguip <aldo.reategui.0298@gmail.com>
2023-05-16 08:54:35 -0500

    add permission dependencies

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,10 @@ export default defineConfig({
                 //
                 "resources/js/app.js",
                 "resources/js/forms.js",
-                "resources/js/role.js",
                 "resources/js/sidebar.js",
+                "resources/js/modules/role.js",
+                "resources/js/modules/user.js",
+                "resources/js/modules/main.js",
                 // "DataTables/datatables.min.js",
                 "resources/css/login_styles.css",
                 "resources/css/main_background.css",
@@ -23,6 +25,7 @@ export default defineConfig({
         alias: {
             "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
             "~bootstrap-icons": path.resolve(__dirname,"node_modules/bootstrap-icons"),
+            "~select": path.resolve(__dirname,"node_modules/select2/dist"),
             "~admin-lte": path.resolve(__dirname, "node_modules/admin-lte"),
             'yajra-laravel-datatables-oracle': path.resolve(__dirname, 'vendor/yajra/laravel-datatables-oracle/src'),
         },
